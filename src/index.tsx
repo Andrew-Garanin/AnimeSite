080f0ac238192a0722ca78855b0eed7d9959ce6a
Andrew-Garanin <andrey-garanin-00@mail.ru>
2021-11-09 00:59:02 +0300

    Added background and change some styles.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,5 @@
 import { Box } from "@mui/system";
-import { constants, kMaxLength } from "buffer";
-import { EWOULDBLOCK, O_DIRECT } from "constants";
-import React, { useState } from "react";
 import ReactDOM from 'react-dom';
-import { convertTypeAcquisitionFromJson, isParameter, ListFormat } from "typescript";
-import { threadId } from "worker_threads";
 import { Body } from "./components/body";
 import { Head } from "./components/header";
 import './index.css';   
@@ -12,10 +7,10 @@ import './index.css';
 
 ReactDOM.render(
     <>
-    <Head/>
-    <Box className = "body_style">
-        <Body className = "cartochka"/>
-    </Box>
+        <Head/>
+        <Box className = "body_style">
+            <Body/>
+        </Box>
     </>,
     document.getElementById('root')
 );
