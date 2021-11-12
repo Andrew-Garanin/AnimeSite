379a0a989a57c11225f63e638100cc42131179e8
Andrew-Garanin <andrey-garanin-00@mail.ru>
2021-11-13 00:37:29 +0300

    Added content to card's dialog.

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -3,9 +3,17 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Dialog from '@mui/material/Dialog';
-import { CardMedia, DialogContent, Grid } from '@mui/material';
-
-
+import { CardMedia, DialogContent, Grid, InputAdornment, TextField } from '@mui/material';
+import LinkIcon from '@mui/icons-material/Link';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import DownloadIcon from '@mui/icons-material/Download';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 export interface MoreDialogProps {
   open: boolean;
   onClose: () => void;
@@ -17,10 +25,18 @@ export interface MoreDialogProps {
 
 const MoreDialog = (props: MoreDialogProps) => {
   const { open, image_reference, title, text, src_reference, onClose } = props;
+  const [mode, setMode] = React.useState("view")
 
   const handleClose = () => {
     onClose();
   };
+
+  const editButton = () => {
+    if (mode == 'view')
+      setMode("edit")
+    else
+      setMode('view')
+  }
 
   return (
     <Dialog
@@ -28,10 +44,14 @@ const MoreDialog = (props: MoreDialogProps) => {
       onClose={handleClose}
       open={open}
       maxWidth="md">
-      <Grid container
-        direction="row">
 
-        <Grid item md={6} >
+      <Grid
+        className="dialog"
+        container
+        direction="row"
+        flexWrap="nowrap">
+
+        <Grid item md={6} xs={8}>
           <DialogContent style={{ height: '80vh', padding: 0 }}>
             <CardMedia
               className="prev_image"
@@ -42,14 +62,96 @@ const MoreDialog = (props: MoreDialogProps) => {
           </DialogContent>
         </Grid>
 
-        <Grid item md={6} >
-          <Typography variant="body2" paddingTop="14px" paddingLeft="14px" paddingRight="14px" className="main_card_text">
-            {title}
-          </Typography>
+
+        <Grid item md={6} xs={8}>
+          <DialogContent style={{ height: '80vh', padding: 0 }}>
+          <Box overflow='auto' height="100%" className='section'>
+            <Grid container
+              direction='column'
+              px={1}
+              spacing={3}
+              height="100%"
+              padding='1vw'
+              wrap="nowrap"
+              >
+            
+              <Grid item>
+                <Button variant="contained"><DownloadIcon /> Save image...</Button>
+                <Button onClick={editButton} variant="contained"><ModeEditOutlineIcon /> Edit mode...</Button>
+              </Grid>
+
+              <Grid item>
+                <Grid container direction='column'>
+
+                  <Grid item>
+                    <LinkIcon />
+                  </Grid>
+
+                  <Grid item>
+                    <a href={src_reference}>{src_reference}</a>
+                    {mode == 'edit' && <TextField multiline rows={3} InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <ModeEditOutlineIcon />
+                        </InputAdornment>
+                      ),
+                    }} size='small' sx={{ width: '100%' }} type='text'></TextField>}
+                  </Grid>
+
+                </Grid>
+              </Grid>
+
+              <Grid item>
+                <Grid container direction='column'>
+
+                  <Grid item>
+                    <PushPinIcon />
+                  </Grid>
+
+                  <Grid item>
+                    <Typography variant="body2" paddingTop="14px" paddingLeft="14px" paddingRight="14px" className="main_card_text">
+                      {title}
+                    </Typography>
+                    {mode == 'edit' && <TextField multiline rows={3} InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <ModeEditOutlineIcon />
+                        </InputAdornment>
+                      ),
+                    }} size='small' sx={{ width: '100%' }} type='text'></TextField>}
+                  </Grid>
+
+                </Grid>
+              </Grid>
+
+              <Grid item>
+                <AutoStoriesIcon />
+                <Typography>
+                  {text}
+                </Typography>
+                {mode == 'edit' && <TextField multiline rows={3} InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <ModeEditOutlineIcon />
+                    </InputAdornment>
+                  ),
+                }} size='small' sx={{ width: '100%' }} type='text'></TextField>}
+              </Grid>
+
+              <Grid item direction="row">
+                <FacebookIcon />
+                <WhatsAppIcon />
+                <TelegramIcon />
+                <PinterestIcon />
+              </Grid>
+              
+            </Grid>
+            </Box>
+          </DialogContent>
         </Grid>
 
-      </Grid>
-    </Dialog>
+      </Grid >
+    </Dialog >
   );
 }
 
@@ -80,6 +182,7 @@ export const MoreButtonDialog = (props: SimpleDialogDemoProps) => {
         size='medium'
         sx={{ width: "95%", color: "#d57eeb", border: '2px solid #d57eeb' }}
         onClick={handleClickOpen}>
+
         More
       </Button>
 
